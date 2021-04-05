e8f60f84770dfc6204dabbdc777b7b8582b0621a
Dmytro Horetskyi <43165664+DmytroHoretskyi@users.noreply.github.com>
2021-04-05 22:50:09 +0300

    changed main.py(added some regex)

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-def read_file(fileName):
-    with open(fileName, 'rb') as a:
-        f = a.readlines()
-        
-    return f
-    
+import re
 
-def main(startDate, endDate, phrase, codeStatus):
+
+def read_file(file_name):
+    with open(file_name) as a:
+        f = a.readlines()
+
+    return f
+
+
+def main(start_date, end_date):
     is_needed_time = False
     line_counter = 0
-    rFile = read_file("access.log")
-    
-    for line in rFile:
-        if endDate in line:
-            is_needed_time = False
+    reading_file = read_file("access.log")
+    for line in reading_file:
+        if end_date in line:
             break
-        if startDate in line:
+        if start_date in line:
             is_needed_time = True
-        if is_needed_time and phrase in line:
-            if codeStatus in line:
-                print(line, "\n")
+        if is_needed_time:
+            if re.findall(r'^([a-zA-Z0-9.]+)( - - )(\[.*?])(\s\"POST.*?\")(\s200\s).*$', line):
+                print(line)
                 line_counter += 1
 
     print(line_counter)
 
 
 if __name__ == '__main__':
-    main(b"[23/Mar/2009:17:18:26", b"[25/Mar/2009:12:12:10", b"POST", b"\" 200")
+    main("[23/Mar/2009:17:18:26", "[25/Mar/2009:12:24:11")
